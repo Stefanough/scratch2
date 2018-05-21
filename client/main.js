6d7ea3b5d407383ec80a6dbbd05f13bec8d4d7e3
Stefan <stefan.armijo@gmail.com>
2018-05-20 20:14:32 -0700

    create main.js with baby react component

---
+++ client/main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+
+  render() {
+    render(
+      <div>
+        hi there whatever.
+      </div>
+    )
+  }
+}
