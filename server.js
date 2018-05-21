b8bd7a7edb388a10fee3e0bc9f7753eeafcf2e7f
Stefan <stefan.armijo@gmail.com>
2018-05-20 19:50:44 -0700

    create client directory and add html css

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const path = require('path');
 
 const PORT = 8080;
 
-app.use(express.static(__dirname));
+// app.get('/', (req, res, next) => {
+//   console.log('in first midware');
+//   res.send('yooooooo');
+// });
+
+app.use(express.static(path.join(__dirname, 'client')));
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
