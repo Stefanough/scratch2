5ec03fb0dc0285a2be0bc5f349d7a5ac49481a6b
Stefan <stefan.armijo@gmail.com>
2018-05-20 21:16:54 -0700

    install babel preset-es2015 and create babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,23 @@ const path = require('path');
 module.exports = {
   entry: './client/main.js',
   output: {
-    filename: 'webpack-bundle.js',
+    // path: '/home/stefan/codesmith/scratch2/build/',
     path: path.resolve(__dirname, 'build'),
+    filename: 'webpack-bundle.js',
+  },
+  devServer: {
+    publicPath: "/build/",
+    port: 8080
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
   }
 };
